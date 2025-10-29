402d83204b34df19a86057b3e65413dadef86b78
JamalEddineEb <jamal-eddine.el-betioui@etu.unice.fr>
2025-10-29 10:39:04 +0100

    login

---
+++ backend/dashboard/src/main/java/com/dashboard/dashboard/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.dashboard.dashboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(authorize -> authorize
+				.anyRequest().authenticated()
+			)
+			.oauth2Login(Customizer.withDefaults())  // enables redirect to Authentik
+			.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())); // optional if you also use API tokens
+		return http.build();
+	}
+
+
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		return JwtDecoders.fromIssuerLocation("http://localhost:9000/application/o/dashboard/");
+	}
+
+}
